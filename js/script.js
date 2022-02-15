65e45df9a807bacc53b2af34361c8d8c512f1c82
Dima <example@gmail.com>
2022-02-15 18:04:16 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,102 @@
+"use strict";
+
+
+// Типы данных
+
+// let number = 5; //number
+
+// console.log(4/0); // Infinity
+// console.log('string' * 9); //NaN
+
+
+// // string
+
+// const persone = 'Alex';
+
+// // boolen
+
+// const bool = false;
+
+// // undefined
+
+// let und;
+// console.log(und); //undefined
+
+// // object
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// // array
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp'];
+
+
+
+
+
+
+// Работа с клиентом
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result); //true/false
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+
+// const answer = [];
+
+// answer[0] = prompt('Как ваше имя', '');
+// answer[1] = prompt('Как ваша Фамилимя', '');
+// answer[2] = +prompt('Сколько вам лет', ''); //унарный плюс
+
+// document.write(answer);
+
+
+
+
+// Интерполяция
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category); //конкотинация строк
+// console.log(`https://someurl.com/${category}/5`); //интерполяция 
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user} `);
+
+
+
+
+// Операторы в JS
+
+// console.log('arr' + ' - object');
+// console.log(typeof(4 + "5"));
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++; //постфиксный (сначала возвращает старое значение, а после увеличивает)
+// --decr; // префиксный
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(5%2); //остаток от деления
+
+// consolr.log(2*4 == 8); //true
+
+// // && - если обе части будут верны
+// // || - хотя бы одно из условий верно
+// const isChecked = true,
+//         isClose = true;
+
+//         console.log(isChecked && isClose);
